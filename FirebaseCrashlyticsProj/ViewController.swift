0386bd3cbf7b9dfe01d4e65edc358fe5c9755d2d
Meet Budheliya <69499558+MeetBudheliya@users.noreply.github.com>
2022-07-15 12:13:30 +0530

    Add files via upload

--- FirebaseCrashlyticsProj/ViewController.swift
+++ FirebaseCrashlyticsProj/ViewController.swift
@@ -13,16 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let button = UIButton(type: .roundedRect)
-             button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
-             button.setTitle("Test Crash", for: [])
-             button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-             view.addSubview(button)
-    }
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-         let numbers = [0]
-         let _ = numbers[1]
-     }
 
+        // See in Keys Section
+//        let keysAndValues = [
+//                         "user" : "meet@gmail.com",
+//                         "user_id" : "1",
+//                         "occupation" : "developer",
+//                         "followers" : 33
+//                        ] as [String : Any]
+//        Crashlytics.crashlytics().setCustomKeysAndValues(keysAndValues)
+
+        // See in Data Section
+        Crashlytics.crashlytics().setUserID("3")
+
+        // Customize Stack Trace
+        let userInfo: [String: String] = [
+            "deviceId": "someDeviceId",
+            "localizedDescription": "yourException.localizedDescription",
+            "anotherKey": "anotherValue"
+        ]
+        let domain = "GET: domain/app/version/api/endpoint"
+        let code = 404
+        let error = NSError(domain: domain, code: code, userInfo: userInfo)
+        Crashlytics.crashlytics().record(error: error)
+
+    }
+
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        fatalError("Test Error")
+     }
 }
 
